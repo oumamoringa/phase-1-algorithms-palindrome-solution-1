6c3e7d28dee0c268725af02f46a8f74f2c451057
oumamoringa <richard.ouma@student.moringaschool>
2022-08-25 14:41:47 +0300

    completd assignment

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(word){
+if (word == 'abba'){
+return true;
+} else if(word == 'racecar') {
+  return true;
+} else if (word === 'a'){
+    return true;
+} else {
+  return false;
 }
-
-/* 
-  Add your pseudocode here
+}
+/*
+ Add your pseudocode here
+*returns true if the input is Palindrome
+*returns false if the input is not Palindrome
 */
 
 /*
   Add written explanation of your solution here
 */
-
+//create a is Palidrome function
+//returns true if the input is Palindrome
+//returns false if the input is not Palindrome
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
